4dc95aff1e4e1087222d257cb402ddd3d1efff8a
cooper <coopergrinnell@gmail.com>
2020-01-16 12:18:09 -0500

    checks for validity

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-year = input("enter year\n")
+year = input("enter year: ")
 #must be between 1900 - 2020
-if int(year) >= 1900 and int(year) <= 2020:
-    print("valid")
+yeari = None
+try:
+    yeari = int(year)
+except:
+    pass
+if yeari is not None:
+
+    if yeari >= 1900:
+        if yeari <= 2020:
+            print(str(yeari) + " is valid :)")
+        else:
+            print("year too big")
+    else:
+        print("year too small")
 else:
-    print("invalid")
+    print(str(year) + " is not valid")
+
