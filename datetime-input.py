a5e41ccb954fb97247f0b590e131bd5f136bb4fb
coopergrinnell <coopergrinnell@gmail.com>
2020-01-22 22:47:03 -0500

    took out some test case things and added "invalid datetime"

--- datetime-input.py
+++ datetime-input.py
@@ -20,63 +20,63 @@ while 1:
     if int(year) >= 1900 and int(year) <= 2020:
         print('good')
     else:
-        print('bad')
+        print('invalid datetime')
         break
     
     month = input("enter month as MM: ")
     if int(month) >= 1 and int(month) <= 12:
         print('good')
     else:
-        print('bad')
+        print('invalid datetime')
         break
     
     day = input("enter day as DD: ")
     if int(month) == 1 or 3 or 5 or 7 or 8 or 10 or 12:
         if int(day) >= 1 and int(day) <= 31:
-            print('good 31')
+            print('good')
         else:
-            print('bad 31')
+            print('invalid datetime')
             break
     if int(month) == 4 or 6 or 9 or 11:
         if int(day) >= 1 and int(day) <= 30:
-            print('good 30')
+            print('good')
         else:
-            print('bad 30')
+            print('invalid datetime 30')
             break
     if int(month) == 2:
         if int(year) % 4 == 0:
             if int(day) >= 1 and int(day) <= 29:
-                print('good 29')
+                print('good')
             else:
-                print('bad')
+                print('invalid datetime')
                 break
         if int(day) >= 1 and int(day) <= 28:
-            print('good 28')
+            print('good')
         else:
-            print('bad')
+            print('invalid datetime')
             break
     else:
-        print('bad')
+        print('invalid datetime')
         break
     
     hour = input("enter hour as HH in 24 hour format: ")
     if int(hour) >= 1 and int(hour) <= 24:
         print('good')
     else:
-        print('bad')
+        print('invalid datetime')
         break
     minute = input("enter minute as MM: ")
     if int(minute) >= 1 and int(minute) <= 60:
         print('good')
     else:
-        print('bad')
+        print('invalid datetime')
         break
 
     second = input("enter seconds as SS: ")
     if int(second) >= 1 and int(second) <= 60:
         print('good')
     else:
-        print('bad')
+        print('invalid datetime')
         break
 
     print(str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":" + str(minute) + ":" + str(second))
